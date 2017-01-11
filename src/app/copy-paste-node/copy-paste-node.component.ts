bc6221268d1fa25903ef02a51788fdd70b0d26a0
Lizzie Szoke <elizabeth.szoke@wipro.com>
2017-01-11 11:40:38 -0700

    add keydown events to copy and paste

--- src/app/copy-paste-node/copy-paste-node.component.ts
+++ src/app/copy-paste-node/copy-paste-node.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, HostListener, OnInit } from '@angular/core';
 import { Map, OrderedMap } from 'immutable';
 import { Subscription } from 'rxjs';
 import { clone } from 'ramda';
@@ -23,12 +23,13 @@ export class CopyPasteNodeComponent implements OnInit {
     currentNode: '',
   };
 
-  constructor(private stateService: StateService, public modalService: NgbModal) {
+  constructor(private stateService: StateService, public modalService: NgbModal, private cd: ChangeDetectorRef) {
   }
 
   ngOnInit() {
     this.stateService.userState.observable.subscribe(response => {
       this.disabled = !response.get('isEditing');
+      this.cd.markForCheck();
       userStateServiceResponseToObject.bind(this)(response);
       if (!this.userState.currentNode) {
         this.userState.currentNode = '';
@@ -38,19 +39,30 @@ export class CopyPasteNodeComponent implements OnInit {
 
   copyNode() {
     this.stateService.userState.setCopiedNodeId();
+    this.subscription = this.stateService.twiglet.nodes.observable.subscribe((response: OrderedMap<string, Map<string, any>>) => {
+      this.node = response.get(this.userState.copiedNodeId).toJS();
+    });
   }
 
   pasteNode() {
     if (this.userState.copiedNodeId) {
-      this.subscription = this.stateService.twiglet.nodes.observable.subscribe((response: OrderedMap<string, Map<string, any>>) => {
-        this.node = response.get(this.userState.copiedNodeId).toJS();
-      });
       const copiedNode = clone(this.node);
       copiedNode.id = UUID.UUID();
       copiedNode.x = copiedNode.x + 25;
       this.stateService.twiglet.nodes.addNode(copiedNode);
       const modelRef = this.modalService.open(EditNodeModalComponent);
       modelRef.componentInstance.id = copiedNode.id;
+    }
+  }
+
+  @HostListener('window:keydown', ['$event'])
+  handleKeyDown($event) {
+    if (($event.metaKey || $event.ctrlKey) && $event.code === 'KeyC' && !this.disabled) {
+      this.copyNode();
+    }
+
+    if (($event.metaKey || $event.ctrlKey) && $event.code === 'KeyV' && !this.disabled) {
+      this.pasteNode();
     }
   }
 
