aa7e374691ac1c470fa91918e4f92222c412ba77
Lizzie Szoke <elizabeth.szoke@wipro.com>
2017-02-21 10:55:45 -0700

    change date format on changelog

--- src/app/changelog-list-twiglet/changelog-list-twiglet.component.ts
+++ src/app/changelog-list-twiglet/changelog-list-twiglet.component.ts
@@ -14,7 +14,7 @@ import { UserState } from './../../non-angular/interfaces/userState/index';
 })
 export class ChangelogListTwigletComponent implements OnInit, OnChanges {
   @Input() twiglet: Map<string, any>;
-  changelog: string[];
+  changelog: any[];
 
   constructor(private stateService: StateService, private cd: ChangeDetectorRef) {
   }
@@ -33,11 +33,6 @@ export class ChangelogListTwigletComponent implements OnInit, OnChanges {
     if (this.twiglet.get('name')) {
       this.stateService.twiglet.changeLogService.getChangelog(this.twiglet.get('name')).subscribe(res => {
         this.changelog = res.changelog;
-        for (let i = 0; i < res.changelog.length; i ++) {
-          console.log(res.changelog[i].timestamp);
-          const date = new Date(res.changelog[i].timestamp);
-          console.log(date);
-        }
         this.cd.markForCheck();
       });
     }
