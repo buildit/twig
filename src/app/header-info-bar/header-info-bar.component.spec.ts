3313c8e0756d1da6328039d7dc299ae2d6ad37ad
Lizzie Szoke <elizabeth.szoke@wipro.com>
2017-02-23 11:52:07 -0700

    fix tests

--- src/app/header-info-bar/header-info-bar.component.spec.ts
+++ src/app/header-info-bar/header-info-bar.component.spec.ts
@@ -1,14 +1,15 @@
-import { Observable } from 'rxjs';
-import { routerForTesting } from './../app.router';
 import { Router } from '@angular/router';
-/* tslint:disable:no-unused-variable */
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
+import { NgbModal, NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { Observable } from 'rxjs';
 
 import { StateService } from '../state.service';
 import { stateServiceStub } from '../../non-angular/testHelpers';
 import { HeaderInfoBarComponent } from './header-info-bar.component';
+import { LoginButtonComponent } from './../login-button/login-button.component';
+import { routerForTesting } from './../app.router';
 
 describe('HeaderInfoBarComponent', () => {
   let component: HeaderInfoBarComponent;
@@ -16,8 +17,10 @@ describe('HeaderInfoBarComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HeaderInfoBarComponent ],
+      declarations: [ HeaderInfoBarComponent, LoginButtonComponent ],
+      imports: [ NgbModule.forRoot() ],
       providers: [
+        NgbModal,
         { provide: StateService, useValue: stateServiceStub()},
         { provide: Router, useValue: { events: Observable.of() } },
       ]
