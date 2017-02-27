f8047198d92501940aa6802ff532b96cf854df19
Ben Hernandez <ben.hernandez@wipro.com>
2017-02-27 11:56:53 -0700

    fixing dropup on font awesome icon picker

--- src/app/font-awesome-icon-picker/font-awesome-icon-picker.component.ts
+++ src/app/font-awesome-icon-picker/font-awesome-icon-picker.component.ts
@@ -110,11 +110,24 @@ export class FontAwesomeIconPickerComponent implements OnInit {
         if (this.show) {
           const buttonRect = this.elementRef.nativeElement.querySelector('button').getBoundingClientRect();
           const dropDownRect = this.elementRef.nativeElement.querySelector('.fa-icon-dropdown').getBoundingClientRect();
-          if (event.clientX < buttonRect.left || event.clientX > buttonRect.right
-              || event.clientY < buttonRect.top || event.clientY > dropDownRect.bottom) {
+          if (event.clientX < buttonRect.left || event.clientX > buttonRect.right) {
             this.show = false;
             removeEventListener();
             this.cd.markForCheck();
+          } else {
+            if (this.dropUp) {
+              if (event.clientY < dropDownRect.top || event.clientY > buttonRect.bottom) {
+                this.show = false;
+                removeEventListener();
+                this.cd.markForCheck();
+              }
+            } else {
+              if (event.clientY < buttonRect.top || event.clientY > dropDownRect.bottom) {
+                this.show = false;
+                removeEventListener();
+                this.cd.markForCheck();
+              }
+            }
           }
         } else {
           removeEventListener();
