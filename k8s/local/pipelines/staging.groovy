6ba6231875520341eba319e8c5b4e9a60dde2922
Ben Hernandez <ben.hernandez@wipro.com>
2017-03-09 10:47:05 -0700

    more testing resources

--- k8s/local/pipelines/staging.groovy
+++ k8s/local/pipelines/staging.groovy
@@ -15,7 +15,7 @@ dockerRegistry = "builditdigital"
 image = "$dockerRegistry/$appName"
 appUrl = 'http://twig2.stage.kube.local'
 
-k8s.build([containerTemplate(name: 'nodejs-builder', image: 'builditdigital/node-builder', ttyEnabled: true, command: 'cat', privileged: true)],
+k8s.build([containerTemplate(name: 'nodejs-builder', image: 'builditdigital/node-builder', ttyEnabled: true, command: 'cat', privileged: true, resourceRequestCpu: "1", resourceRequestMemory: "2048m")],
   [hostPathVolume(mountPath: '/var/projects', hostPath: '/Users/benhernandez/dev/projects')]) {
 
   try {
@@ -36,14 +36,14 @@ k8s.build([containerTemplate(name: 'nodejs-builder', image: 'builditdigital/node
         }
       }
 
-      // stage("Test") {
-      //   try {
-      //     sh "npm run test:ci"
-      //   }
-      //   finally {
-      //     junit 'reports/test-results.xml'
-      //   }
-      // }
+      stage("Test") {
+        try {
+          sh "npm run test:ci"
+        }
+        finally {
+          junit 'reports/test-results.xml'
+        }
+      }
 
       stage("Analysis") {
         sh "npm run lint"
