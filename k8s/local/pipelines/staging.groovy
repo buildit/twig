d7f41dea4f0d6f70c4605aec5caa1361097c1aea
Ben Hernandez <ben.hernandez@wipro.com>
2017-03-09 11:28:29 -0700

    k8 stuff

--- k8s/local/pipelines/staging.groovy
+++ k8s/local/pipelines/staging.groovy
@@ -10,12 +10,12 @@ buildNumber = env.BUILD_NUMBER
 appName = "twig2"
 targetEnv = "staging"
 slackChannel = "twig2"
-gitUrl = "https://github.com/buildit/twig.git"
+gitUrl = "https://github.com/buildit/twig2.git"
 dockerRegistry = "builditdigital"
 image = "$dockerRegistry/$appName"
 appUrl = 'http://twig2.stage.kube.local'
 
-k8s.build([containerTemplate(name: 'nodejs-builder', image: 'builditdigital/node-builder', ttyEnabled: true, command: 'cat', privileged: true)],
+k8s.build([containerTemplate(name: 'nodejs-builder', image: 'builditdigital/node-builder', ttyEnabled: true, command: 'cat', privileged: true, resourceRequestCpu: "1", resourceRequestMemory: "2048m")],
   [hostPathVolume(mountPath: '/var/projects', hostPath: '/Users/benhernandez/dev/projects')]) {
 
   try {
@@ -41,7 +41,7 @@ k8s.build([containerTemplate(name: 'nodejs-builder', image: 'builditdigital/node
       //     sh "npm run test:ci"
       //   }
       //   finally {
-      //     junit './reports/test-results.xml'
+      //     junit 'reports/test-results.xml'
       //   }
       // }
 
