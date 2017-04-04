cafe4eee95a84711f64e230fa07056587a233615
Lizzie Szoke <elizabeth.szoke@wipro.com>
2017-04-04 12:33:26 -0600

    remove extra css

--- src/app/core/splash/splash.component.ts
+++ src/app/core/splash/splash.component.ts
@@ -6,10 +6,12 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './splash.component.html',
 })
 export class SplashComponent implements OnInit {
+  public splashImage: string;
 
   constructor() { }
 
   ngOnInit() {
+    this.splashImage = '../../../../assets/images/twig-splash.png';
   }
 
 }
