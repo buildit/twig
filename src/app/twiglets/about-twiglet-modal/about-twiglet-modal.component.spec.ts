934feb2b7f656a6d6878eafc2931612a11210bb2
Lizzie Szoke <elizabeth.szoke@wipro.com>
2017-04-11 15:49:54 -0600

    fix tests

--- src/app/twiglets/about-twiglet-modal/about-twiglet-modal.component.spec.ts
+++ src/app/twiglets/about-twiglet-modal/about-twiglet-modal.component.spec.ts
@@ -1,25 +1,69 @@
+import { Twiglet } from './../../../non-angular/interfaces/twiglet/twiglet';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule, ReactiveFormsModule, FormControl } from '@angular/forms';
+import { NgbActiveModal, NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AboutTwigletModalComponent } from './about-twiglet-modal.component';
+import { StateService } from './../../state.service';
+import { stateServiceStub } from '../../../non-angular/testHelpers';
 
 describe('AboutTwigletModalComponent', () => {
   let component: AboutTwigletModalComponent;
   let fixture: ComponentFixture<AboutTwigletModalComponent>;
+  let compRef;
+  const stateServiceStubbed = stateServiceStub();
+  stateServiceStubbed.twiglet.loadTwiglet('name1');
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ AboutTwigletModalComponent ]
+      declarations: [ AboutTwigletModalComponent ],
+      imports: [ FormsModule, NgbModule.forRoot(), ReactiveFormsModule ],
+      providers: [
+        { provide: StateService, useValue: stateServiceStubbed },
+        NgbActiveModal
+      ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(AboutTwigletModalComponent);
+    compRef = fixture.componentRef.hostView['internalView']['compView_0'];
     component = fixture.componentInstance;
+    component.twigletName = 'name1';
+    component.description = 'This is the description about the Twiglet.';
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should switch to edit mode when edit icon is clicked', () => {
+    fixture.nativeElement.querySelector('.fa-pencil').click();
+    expect(component.editMode).toEqual(true);
+  });
+
+  it('form has the correct description', () => {
+    expect(component.form.controls.description.value).toEqual('This is the description about the Twiglet.');
+  });
+
+  describe('process form', () => {
+    beforeEach(() => {
+      fixture.nativeElement.querySelector('.fa-pencil').click();
+      fixture.detectChanges();
+    });
+
+    it('sets the description when save is clicked', () => {
+      spyOn(component.stateService.twiglet, 'setDescription');
+      fixture.nativeElement.querySelector('.save').click();
+      expect(component.stateService.twiglet.setDescription).toHaveBeenCalled();
+    });
+
+    it('saves the updated twiglet', () => {
+      spyOn(stateServiceStubbed.twiglet, 'saveChanges').and.returnValue({ subscribe: () => {} });
+      fixture.nativeElement.querySelector('.save').click();
+      expect(component.stateService.twiglet.saveChanges).toHaveBeenCalled();
+    });
   });
 });
--- src/app/twiglets/about-twiglet-modal/about-twiglet-modal.component.ts
+++ src/app/twiglets/about-twiglet-modal/about-twiglet-modal.component.ts
@@ -1,25 +1,10 @@
-import {
-  AfterViewChecked,
-  ChangeDetectionStrategy,
-  ChangeDetectorRef,
-  Component,
-  Input,
-  OnChanges,
-  OnDestroy,
-  OnInit,
-  SimpleChanges
-} from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 import { Http } from '@angular/http';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
-import { List, Map } from 'immutable';
-import { ToastsManager } from 'ng2-toastr/ng2-toastr';
-import { Subscription } from 'rxjs/Subscription';
 
 import { handleError } from '../../../non-angular/services-helpers/httpHelpers';
 import { StateService } from './../../state.service';
-import { Twiglet } from './../../../non-angular/interfaces/twiglet/twiglet';
-import { TwigletService } from './../../../non-angular/services-helpers/twiglet/index';
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -27,23 +12,14 @@ import { TwigletService } from './../../../non-angular/services-helpers/twiglet/
   styleUrls: ['./about-twiglet-modal.component.scss'],
   templateUrl: './about-twiglet-modal.component.html',
 })
-export class AboutTwigletModalComponent implements OnInit, AfterViewChecked, OnDestroy {
+export class AboutTwigletModalComponent implements OnInit {
   twigletName: string;
   description: string;
   editMode = false;
-  twigletService: TwigletService;
-  twigletServiceSubsciption: Subscription;
   form: FormGroup;
 
-  constructor(private fb: FormBuilder, private stateService: StateService, private cd: ChangeDetectorRef,
-    private activeModal: NgbActiveModal, private toastr: ToastsManager) {
-      this.twigletService = new TwigletService(
-        stateService.http,
-        stateService.toastr,
-        stateService.router,
-        stateService.modalService,
-        false,
-        stateService.userState, null);
+  constructor(private fb: FormBuilder, public stateService: StateService,
+    public activeModal: NgbActiveModal) {
     }
 
   ngOnInit() {
@@ -59,7 +35,6 @@ export class AboutTwigletModalComponent implements OnInit, AfterViewChecked, OnD
   }
 
   processForm() {
-    console.log(this.form.controls.description.value);
     this.stateService.twiglet.setDescription(this.form.controls.description.value);
     this.stateService.twiglet.saveChanges('description updated')
       .subscribe(response => {
@@ -67,12 +42,6 @@ export class AboutTwigletModalComponent implements OnInit, AfterViewChecked, OnD
         this.stateService.twiglet.changeLogService.refreshChangelog();
         this.activeModal.close();
       }, handleError);
-  }
-
-  ngOnDestroy() {
-  }
-
-  ngAfterViewChecked() {
   }
 
 }
