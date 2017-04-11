5186f2d4e02f4197b0735714fdb0fb06c16cd3c9
Lizzie Szoke <elizabeth.szoke@wipro.com>
2017-04-11 12:46:17 -0600

    fix tests

--- src/app/core/ping/ping.component.spec.ts
+++ src/app/core/ping/ping.component.spec.ts
@@ -1,14 +1,18 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { NgbActiveModal, NgbModal, NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { Map } from 'immutable';
 
 import { PingComponent } from './ping.component';
 
-fdescribe('PingComponent', () => {
+describe('PingComponent', () => {
   let component: PingComponent;
   let fixture: ComponentFixture<PingComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ PingComponent ]
+      declarations: [ PingComponent ],
+      imports: [ NgbModule.forRoot() ],
+      providers: [ NgbActiveModal ]
     })
     .compileComponents();
   }));
@@ -16,6 +20,25 @@ fdescribe('PingComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(PingComponent);
     component = fixture.componentInstance;
+    component.userState = Map({
+      ping: {
+        authenticated: {
+          user: {
+            name: 'some@email.com'
+          }
+        },
+        config: {
+          DB_URL: 'dburl',
+          LDAP_URL: 'ldap://url',
+          LOG_CONSOLE: true,
+          LOG_FILE: true,
+          LOG_LEVEL: 'debug',
+          TENANT: ''
+        },
+        version: '2.0.0',
+      },
+      user: 'some@email.com',
+    });
     fixture.detectChanges();
   });
 
