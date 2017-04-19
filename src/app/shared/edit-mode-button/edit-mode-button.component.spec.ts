e88b1c3035994c68754d3bbeec70e34d9b88ddb4
Lizzie Szoke <elizabeth.szoke@wipro.com>
2017-04-19 11:26:01 -0600

    add edit mode button tests

--- src/app/shared/edit-mode-button/edit-mode-button.component.spec.ts
+++ src/app/shared/edit-mode-button/edit-mode-button.component.spec.ts
@@ -1,4 +1,3 @@
-import { router } from './../../app.router';
 import { DebugElement } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
