6eb14b4821ccda46d2e6060bec013f0d40c05777
Ben Hernandez <ben.hernandez@wipro.com>
2017-04-21 12:30:04 -0600

    adding screenshots to protractor

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,23 +1,31 @@
 // Protractor configuration file, see link for more information
 // https://github.com/angular/protractor/blob/master/docs/referenceConf.js
+/* eslint import/no-extraneous-dependencies: 0 */
 
 /* global jasmine */
 const { SpecReporter } = require('jasmine-spec-reporter');
 const reporters = require('jasmine-reporters');
+const tsNode = require('ts-node');
+const HtmlScreenshotReporter = require('protractor-jasmine2-screenshot-reporter');
+
+const screenShotReporter = new HtmlScreenshotReporter({
+  dest: './screenshots',
+  captureOnlyFailedSpecs: true,
+});
 
 const junitReporter = new reporters.JUnitXmlReporter({
   savePath: 'reports/acceptance-test-results.xml',
-  consolidateAll: false
+  consolidateAll: false,
 });
 
 exports.config = {
   allScriptsTimeout: 60000,
   specs: [
     'node_modules/jasmine-expect/index.js',
-    './e2e/**/*.e2e-spec.ts'
+    './e2e/**/*.e2e-spec.ts',
   ],
   capabilities: {
-    'browserName': 'chrome'
+    browserName: 'chrome',
   },
   directConnect: true,
   baseUrl: 'http://localhost:4200/',
@@ -25,16 +33,25 @@ exports.config = {
   jasmineNodeOpts: {
     showColors: true,
     defaultTimeoutInterval: 60000,
-    print: function() {}
+    print() {},
   },
-  beforeLaunch: function() {
-    require('ts-node').register({
+  beforeLaunch() {
+    tsNode.register({
       project: 'e2e',
+    });
+    return new Promise((resolve) => {
+      screenShotReporter.beforeLaunch(resolve);
     });
   },
   onPrepare() {
     jasmine.DEFAULT_TIMEOUT_INTERVAL = 60000;
     jasmine.getEnv().addReporter(new SpecReporter({ spec: { displayStacktrace: true } }));
     jasmine.getEnv().addReporter(junitReporter);
+    jasmine.getEnv().addReporter(screenShotReporter);
+  },
+  afterLaunch(exitCode) {
+    return new Promise((resolve) => {
+      screenShotReporter.afterLaunch(resolve.bind(this, exitCode));
+    });
   },
 };
