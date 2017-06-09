390e18b09282085ee030fa86f29c76af516a7ab8
Andrew Ochsner <andrew.ochsner@wipro.com>
2017-06-08 22:38:45 -0500

    oops wrong replacement

--- src/non-angular/config.ts
+++ src/non-angular/config.ts
@@ -4,7 +4,7 @@ export class Config {
       return `${window.location.protocol}//localhost:3000/v2`;
     }
     let twigApiHostname = window.location.hostname.replace('twig', 'twig-api');
-    twigApiHostname = window.location.hostname.replace('twig2', 'twig-api');
+    twigApiHostname = twigApiHostname.replace('twig2', 'twig-api');
     return `${window.location.protocol}//${twigApiHostname}/v2`;
   };
   static modelsFolder = 'models';
