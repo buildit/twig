98bd9fba93fecc1902dcc3e63258b4110f9d4f57
Ben Hernandez <ben.hernandez@wipro.com>
2017-08-30 08:42:20 -0600

    more unit fixes

--- src/app/core/login-modal/login-modal.component.ts
+++ src/app/core/login-modal/login-modal.component.ts
@@ -59,9 +59,7 @@ export class LoginModalComponent implements OnInit, OnDestroy {
       this.redirectionSubscription = Observable.interval(100).subscribe(x => {
         this.redirectionMessage = `Redirecting.${range(0, x % 3).reduce((s) => `${s}.`, '')}`;
       });
-      window.location.href = 'https://login.microsoftonline.com/258ac4e4-146a-411e-9dc8-79a9e12fd6da/oauth2/' +
-        `authorize?client_id=ce2abe9c-2019-40b2-8fbc-651a6157e956&redirect_uri=${rootUrl}` +
-        `&state=${encodeURIComponent(this.router.url)}&response_type=id_token&nonce=${UUID.UUID()}`;
+      this.redirectToAdLogin();
     }
   }
 
