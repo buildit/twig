41bbac184b8e76c001d1f73e428fb68240091f8b
Andrew Ochsner <aochsner@cs.stanford.edu>
2017-09-05 15:25:47 -0500

    Add Jenkinsfile (#38)
    * Add Jenkinsfile
    
    * for now skip validate until bootstrap can split it's dependencies

--- pipelines/production.groovy
+++ pipelines/production.groovy
@@ -1,81 +1,70 @@
-@Library('buildit')
-def LOADED = true
+@Library('buildit') _
+def appName = 'twig2'
+def registryBase = "006393696278.dkr.ecr.${env.AWS_REGION}.amazonaws.com"
+def tag = 'latest'
+def gitUrl = "https://github.com/buildit/twig"
+def appUrl = "https://twig.buildit.tools"
+def slackChannel = "twig"
 
-// Production release pipeline
-node {
-
-  currentBuild.result = "SUCCESS"
-
-  try {
-
-    stage("Set Up") {
-      sendNotifications = !env.DEV_MODE
-      ad_ip_address = sh(script: "dig +short corp.${env.RIG_DOMAIN} | head -1", returnStdout: true).trim()
-
-      checkout scm
-      // clean the workspace before checking out
-      sh "git clean -ffdx"
-
-      if (env.USE_GLOBAL_LIB) {
-        uiInst = new ui()
-        ecrInst = new ecr()
-        slackInst = new slack()
-        templateInst = new template()
-        convoxInst = new convox()
-      } else {
-        sh "curl -L https://dl.bintray.com/buildit/maven/jenkins-pipeline-libraries-${env.PIPELINE_LIBS_VERSION}.zip -o lib.zip && echo 'A' | unzip -o lib.zip"
-        uiInst = load "lib/ui.groovy"
-        ecrInst = load "lib/ecr.groovy"
-        slackInst = load "lib/slack.groovy"
-        templateInst = load "lib/template.groovy"
-        convoxInst = load "lib/convox.groovy"
-      }
-
-
-      registryBase = "006393696278.dkr.ecr.${env.AWS_REGION}.amazonaws.com"
-      registry = "https://${registryBase}"
-      appUrl = "https://twig.buildit.tools/"
-      appName = "twig2"
-      slackChannel = "twig"
-      gitUrl = "https://github.com/buildit/twig"
-
-      tmpFile = UUID.randomUUID().toString() + ".tmp"
-
-      // select the tag
-      // tag = uiInst.selectTag(ecrInst.imageTags(appName, env.AWS_REGION))
-      tag = "latest"
-    }
-
-    stage("Write docker-compose") {
-      def ymlData = templateInst.transform(readFile("docker-compose.yml.template")
-        , [tag: tag, registryBase: registryBase, ad_ip_address: ad_ip_address])
-
-      writeFile(file: tmpFile, text: ymlData)
-    }
-
+pipeline {
+  agent any
+  options {
+    buildDiscarder(logRotator(numToKeepStr: '10'))
+    disableConcurrentBuilds()
+    skipStagesAfterUnstable()
+  }
+  stages {
     stage("Deploy to production") {
-      convoxInst.login("${env.CONVOX_RACKNAME}")
-      convoxInst.ensureApplicationCreated("${appName}")
-      sh "convox deploy --app ${appName} --description '${tag}' --file ${tmpFile} --wait"
-      sh "rm ${tmpFile}"
-      // wait until the app is deployed
-      convoxInst.waitUntilDeployed("${appName}")
-      convoxInst.ensureSecurityGroupSet("${appName}", "")
-      convoxInst.ensureCertificateSet("${appName}", "nginx", 443, "acm-b53eb2937b23")
-      convoxInst.ensureParameterSet("${appName}", "Internal", "No")
+      steps {
+        script {
+          def templateInst = new template()
+          def convoxInst = new convox()
+          def tmpFile = UUID.randomUUID().toString() + ".tmp"
+          def ymlData = templateInst.transform(readFile("docker-compose.yml.template")
+            , [tag: tag, registryBase: registryBase])
+
+          writeFile(file: tmpFile, text: ymlData)
+
+          convoxInst.login("${env.CONVOX_RACKNAME}")
+          convoxInst.ensureApplicationCreated("${appName}")
+          sh "convox deploy --app ${appName} --description '${tag}' --file ${tmpFile} --wait"
+          sh "rm ${tmpFile}"
+          // wait until the app is deployed
+          convoxInst.waitUntilDeployed("${appName}")
+          convoxInst.ensureSecurityGroupSet("${appName}", "")
+          convoxInst.ensureCertificateSet("${appName}", "nginx", 443, "acm-b53eb2937b23")
+          convoxInst.ensureParameterSet("${appName}", "Internal", "No")
+        }
+      }
     }
   }
-  catch (err) {
-    currentBuild.result = "FAILURE"
-    throw err
-  }
-  finally {
-    if (sendNotifications) {
-      if (currentBuild.result == null || currentBuild.result == "SUCCESS") {
-        slackInst.notify("Deployed to Production", "Tag '<${gitUrl}/commits/tag/${tag}|${tag}>' has been deployed to <${appUrl}|${appUrl}>", "good", "http://i296.photobucket.com/albums/mm200/kingzain/the_eye_of_sauron_by_stirzocular-d86f0oo_zpslnqbwhv2.png", slackChannel)
-      } else {
-        slackInst.notify("Error while deploying to Production", "Tag '<${gitUrl}/commits/tag/${tag}|${tag}>' failed to deploy to <${appUrl}|${appUrl}>", "danger", "http://i296.photobucket.com/albums/mm200/kingzain/the_eye_of_sauron_by_stirzocular-d86f0oo_zpslnqbwhv2.png", slackChannel)
-      }
+  post {
+    success {
+      slackNotify(
+        "Deployed to Production",
+        "(<${env.BUILD_URL}|Job>) Tagged Docker Image '${tag}' has been deployed to <${appUrl}|${appUrl}>",
+        "good",
+        "http://i296.photobucket.com/albums/mm200/kingzain/the_eye_of_sauron_by_stirzocular-d86f0oo_zpslnqbwhv2.png",
+        slackChannel
+      )
+    }
+    failure {
+      slackNotify(
+        "Failed to deploy to Production",
+        "(<${env.BUILD_URL}|Failed Job>)",
+        "danger",
+        "http://i296.photobucket.com/albums/mm200/kingzain/the_eye_of_sauron_by_stirzocular-d86f0oo_zpslnqbwhv2.png",
+        slackChannel
+      )
+    }
+    unstable {
+      slackNotify(
+        "Failed to deploy to Production",
+        "(<${env.BUILD_URL}|Failed Job>)",
+        "danger",
+        "http://i296.photobucket.com/albums/mm200/kingzain/the_eye_of_sauron_by_stirzocular-d86f0oo_zpslnqbwhv2.png",
+        slackChannel
+      )
     }
   }
 }
