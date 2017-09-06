db3dc4daa307bb402bc0efd3924f3ec0b12f3b4c
Lizzie Szoke <elizabeth.szoke@wipro.com>
2017-09-06 09:18:53 -0600

    fix button from ngSubmit

--- src/app/twiglets/edit-node-modal/edit-node-modal.component.spec.ts
+++ src/app/twiglets/edit-node-modal/edit-node-modal.component.spec.ts
@@ -76,7 +76,7 @@ describe('EditNodeModalComponent', () => {
 
     it('displays all of the appropriate select values for node.type and gravity points', () => {
       const selects = fixture.nativeElement.querySelectorAll('option');
-      expect(selects.length).toEqual(9);
+      expect(selects.length).toEqual(3);
     });
 
     it('does not show an error message when the form is valid', () => {
@@ -133,9 +133,7 @@ describe('EditNodeModalComponent', () => {
         attrs.push(component.createAttribute({ key: 'one', value: 'whatever' }));
         attrs.push(component.createAttribute());
         attrs.push(component.createAttribute({ key: 'three', value: 'idk' }));
-        component.form.controls['color'].setValue('purple');
         const expectedNode = {
-          _color: 'purple',
           attrs: [
             { key: 'keyOne', value: 'valueOne', dataType: 'string', required: true },
             { key: 'keyExtra', value: '', dataType: 'string', required: false },
@@ -143,7 +141,6 @@ describe('EditNodeModalComponent', () => {
             { key: 'one', value: 'whatever', dataType: null, required: null },
             { key: 'three', value: 'idk', dataType: null, required: null }
           ],
-          color: 'purple',
           gravityPoint: '',
           id: 'firstNode',
           name: 'a name',
