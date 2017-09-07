29d4ad6be4932736654355aa7d9a477e8a5b1a98
Ben Hernandez <ben.hernandez@wipro.com>
2017-09-07 11:56:28 -0600

    adding tests around add/delete buttons on gravity points list

--- src/app/twiglets/gravity-list/gravity-list.component.spec.ts
+++ src/app/twiglets/gravity-list/gravity-list.component.spec.ts
@@ -20,14 +20,106 @@ describe('GravityListComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(GravityListComponent);
     component = fixture.componentInstance;
+  });
+
+  it('should be created', () => {
     component.userState = fromJS({
       gravityPoints: {},
       user: '',
     });
     fixture.detectChanges();
+    expect(component).toBeTruthy();
   });
 
-  it('should be created', () => {
-    expect(component).toBeTruthy();
+  describe('add gravity button', () => {
+    it('allows adding gravity points if in gravity edit mode and has a user', () => {
+      component.userState = fromJS({
+        gravityPoints: [
+          {
+            id: 'id1',
+            name: 'name1',
+          }
+        ],
+        isEditingGravity: true,
+        user: { }
+      });
+      fixture.detectChanges();
+      expect(fixture.nativeElement.querySelector('i.fa-plus')).toBeTruthy();
+    });
+
+    it('cannot add gravity points if there is no user', () => {
+      component.userState = fromJS({
+        gravityPoints: [
+          {
+            id: 'id1',
+            name: 'name1',
+          }
+        ],
+        isEditingGravity: true,
+      });
+      fixture.detectChanges();
+      expect(fixture.nativeElement.querySelector('i.fa-plus')).toBeFalsy();
+    })
+
+    it('cannot add gravity points if not in gravity edit mode', () => {
+      component.userState = fromJS({
+        gravityPoints: [
+          {
+            id: 'id1',
+            name: 'name1',
+          }
+        ],
+        isEditingGravity: false,
+        user: { },
+      });
+      fixture.detectChanges();
+      expect(fixture.nativeElement.querySelector('i.fa-plus')).toBeFalsy();
+    });
+  })
+
+  describe('deleting gravity points', () => {
+    it('allows deleting gravity points if in gravity edit mode and has a user', () => {
+      component.userState = fromJS({
+        gravityPoints: [
+          {
+            id: 'id1',
+            name: 'name1',
+          }
+        ],
+        isEditingGravity: true,
+        user: { }
+      });
+      fixture.detectChanges();
+      expect(fixture.nativeElement.querySelector('i.fa-trash')).toBeTruthy();
+    });
+
+    it('does not allow deleting gravity points if there is no user', () => {
+      component.userState = fromJS({
+        gravityPoints: {
+          gp1: {
+            id: 'id1',
+            name: 'name1',
+          }
+        },
+        isEditingGravity: true,
+      });
+      fixture.detectChanges();
+      expect(fixture.nativeElement.querySelector('i.fa-trash')).toBeFalsy();
+    });
+
+    it('does not allow deleting gravity points if not in gravity edit mode', () => {
+      component.userState = fromJS({
+        gravityPoints: {
+          gp1: {
+            id: 'id1',
+            name: 'name1',
+          }
+        },
+        isEditingGravity: false,
+        user: {}
+      });
+      fixture.detectChanges();
+      expect(fixture.nativeElement.querySelector('i.fa-trash')).toBeFalsy();
+    });
   });
 });
