f135b4dd26530837b42ab183f752ca966b458b37
Ben Hernandez <ben.hernandez@wipro.com>
2017-09-27 10:17:10 -0600

    fixing test coverage

--- src/app/twiglets/simulation-controls/simulation-controls.component.spec.ts
+++ src/app/twiglets/simulation-controls/simulation-controls.component.spec.ts
@@ -8,6 +8,7 @@ import { SimulationControlsComponent } from './simulation-controls.component';
 import { SliderWithLabelComponent } from '../../shared/slider-with-label/slider-with-label.component';
 import { StateService } from '../../state.service';
 import { stateServiceStub } from '../../../non-angular/testHelpers';
+import USERSTATE from '../../../non-angular/services-helpers/userState/constants';
 
 describe('SimulationControlsComponent', () => {
   let component: SimulationControlsComponent;
@@ -26,11 +27,37 @@ describe('SimulationControlsComponent', () => {
     fixture = TestBed.createComponent(SimulationControlsComponent);
     component = fixture.componentInstance;
     component.viewData = fromJS({});
-    component.userState = fromJS({});
-    fixture.detectChanges();
   });
 
   it('should create', () => {
+    component.userState = fromJS({});
+    fixture.detectChanges();
     expect(component).toBeTruthy();
+  });
+
+  it('enables the controls if there is no view loaded', () => {
+    component.userState = fromJS({});
+    fixture.detectChanges();
+    const slider = <HTMLElement>fixture.nativeElement.querySelector('app-slider-with-label');
+    expect(slider.attributes.getNamedItem('ng-reflect-disabled').value).toEqual('false');
+  });
+
+  it('disables the controls if there is a view loaded', () => {
+    component.userState = fromJS({
+      [USERSTATE.CURRENT_VIEW_NAME]: 'a view'
+    });
+    fixture.detectChanges();
+    const slider = <HTMLElement>fixture.nativeElement.querySelector('app-slider-with-label');
+    expect(slider.attributes.getNamedItem('ng-reflect-disabled').value).toEqual('true');
+  });
+
+  it('enables the controls if there is a view loaded but the user is in edit mode', () => {
+    component.userState = fromJS({
+      [USERSTATE.CURRENT_VIEW_NAME]: 'a view',
+      [USERSTATE.IS_EDITING_VIEW]: true,
+    });
+    fixture.detectChanges();
+    const slider = <HTMLElement>fixture.nativeElement.querySelector('app-slider-with-label');
+    expect(slider.attributes.getNamedItem('ng-reflect-disabled').value).toEqual('false');
   });
 });
