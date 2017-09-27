ed48020da65abad3ee2f8791c124e0711a4ecbcf
Lizzie Szoke <elizabeth.szoke@wipro.com>
2017-09-27 14:39:35 -0600

    add test coverage

--- src/app/twiglets/twiglet-gravity/twiglet-gravity.component.spec.ts
+++ src/app/twiglets/twiglet-gravity/twiglet-gravity.component.spec.ts
@@ -8,6 +8,7 @@ import { StateService } from '../../state.service';
 import { stateServiceStub } from '../../../non-angular/testHelpers';
 import { ToggleButtonComponent } from '../../shared/toggle-button/toggle-button.component';
 import { GravityListComponent } from './../gravity-list/gravity-list.component';
+import USERSTATE from '../../../non-angular/services-helpers/userState/constants';
 
 describe('TwigletGravityComponent', () => {
   let component: TwigletGravityComponent;
@@ -26,12 +27,37 @@ describe('TwigletGravityComponent', () => {
     fixture = TestBed.createComponent(TwigletGravityComponent);
     component = fixture.componentInstance;
     component.viewData = fromJS({});
-    component.userState = fromJS({});
-    fixture.detectChanges();
   });
 
   it('should create', () => {
+    component.userState = fromJS({});
     fixture.detectChanges();
     expect(component).toBeTruthy();
+  });
+
+  it('enables the controls if there is no view loaded', () => {
+    component.userState = fromJS({});
+    fixture.detectChanges();
+    const toggle = <HTMLElement>fixture.nativeElement.querySelector('app-toggle-button');
+    expect(toggle.attributes.getNamedItem('ng-reflect-disabled').value).toEqual('false');
+  });
+
+  it('disables the controls if there is a view loaded', () => {
+    component.userState = fromJS({
+      [USERSTATE.CURRENT_VIEW_NAME]: 'a view'
+    });
+    fixture.detectChanges();
+    const toggle = <HTMLElement>fixture.nativeElement.querySelector('app-toggle-button');
+    expect(toggle.attributes.getNamedItem('ng-reflect-disabled').value).toEqual('true');
+  });
+
+  it('enables the controls if there is a view loaded but the user is in edit mode', () => {
+    component.userState = fromJS({
+      [USERSTATE.CURRENT_VIEW_NAME]: 'a view',
+      [USERSTATE.IS_EDITING_VIEW]: true,
+    });
+    fixture.detectChanges();
+    const toggle = <HTMLElement>fixture.nativeElement.querySelector('app-toggle-button');
+    expect(toggle.attributes.getNamedItem('ng-reflect-disabled').value).toEqual('false');
   });
 });
