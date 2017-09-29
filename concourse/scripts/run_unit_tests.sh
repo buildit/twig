fbf9f95c392fbf667f3edbdf19736c7f86fa3329
Ben Hernandez <ben.hernandez@wipro.com>
2017-09-29 15:43:31 -0600

    trying xvfb

--- concourse/scripts/run_unit_tests.sh
+++ concourse/scripts/run_unit_tests.sh
@@ -3,4 +3,4 @@
 set -e -u -x
 
 mv dependency-cache/node_modules twig
-cd twig && npm rebuild node-sass --force && npm test
+cd twig && npm rebuild node-sass --force && CHROME_BIN=/usr/bin/google-chrome xvfb-run -s '-screen 0 1280x1024x16' npm run test:ci
