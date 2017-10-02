3418d79181c874841e52887d08b075d59486c8e2
Ben Hernandez <ben.hernandez@wipro.com>
2017-10-02 15:46:01 -0600

    whatever

--- concourse/scripts/build.sh
+++ concourse/scripts/build.sh
@@ -2,5 +2,6 @@
 
 set -e -u -x
 
+ls -la
 mv dependency-cache/node_modules twig
-cd twig && npm rebuild node-sass --force && npm run build:prod
+cd twig && npm rebuild node-sass --force && npm run build:prod && cd ..
