e63b4f36b2989e59e33f1fd60220d3b858267e2d
Ben Hernandez <ben.hernandez@wipro.com>
2017-10-03 10:57:00 -0600

    test

--- concourse/scripts/deploy_docker.sh
+++ concourse/scripts/deploy_docker.sh
@@ -6,4 +6,6 @@ rev="$(git rev-parse HEAD | cut -c 1-7)"
 branch="$(git rev-parse --abbrev-ref HEAD)"
 docker_tag=$branch-$rev
 
+echo $DOCKER_USERNAME
+
 cd twig && docker build -t benaychh/twig:$docker_tag && docker images
