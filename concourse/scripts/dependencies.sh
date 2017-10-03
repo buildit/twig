325fa80bb4383ae688342bdaf6829413b672a0cc
Ben Hernandez <ben.hernandez@wipro.com>
2017-10-03 09:13:35 -0600

    blah

--- concourse/scripts/dependencies.sh
+++ concourse/scripts/dependencies.sh
@@ -3,4 +3,4 @@
 set -e -u -x
 
 mv dependency-cache/node_modules twig
-cd twig && npm rebuild node-sass --force
+cd twig && npm rebuild node-sass --force && cd .. && mv twig twig-with-dependencies
--- concourse/scripts/docker_deploy.sh
+++ concourse/scripts/docker_deploy.sh
@@ -6,4 +6,4 @@ rev="$(git rev-parse HEAD | cut -c 1-7)"
 branch="$(git rev-parse --abbrev-ref HEAD)"
 docker_tag=$branch-$rev
 
-cd twig && docker build -t benaychh/twig:$docker_tag && docker images
+cd built-twig && docker build -t benaychh/twig:$docker_tag && docker images
