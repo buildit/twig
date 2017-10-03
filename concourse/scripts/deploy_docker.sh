99421a1f72e68a359750b9e2fe40d63693d02c21
Ben Hernandez <ben.hernandez@wipro.com>
2017-10-03 11:03:06 -0600

    test

--- concourse/scripts/deploy_docker.sh
+++ concourse/scripts/deploy_docker.sh
@@ -8,4 +8,6 @@ docker_tag=$branch-$rev
 
 echo $DOCKER_USERNAME
 
+echo $DOCKER_PASSWORD | docker login --username $DOCKER_USERNAME --password-stdin
+
 cd twig && docker build -t benaychh/twig:$docker_tag && docker images
