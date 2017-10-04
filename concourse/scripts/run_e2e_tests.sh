fe90b193f2829c39ec57c78209579514b4023e69
Ben Hernandez <ben.hernandez@wipro.com>
2017-10-04 13:19:50 -0600

    test

--- concourse/scripts/run_e2e_tests.sh
+++ concourse/scripts/run_e2e_tests.sh
@@ -1,14 +1,5 @@
 #!/bin/sh
 
-# Install docker-compose:
-docker_install(){
-    apk add --no-cache py-pip curl
-    pip install docker-compose
-}
-‌‌
-‌‌
-docker_install
-
 docker-compose -f ./twig-with-deps/concourse/compose/e2e.yml up -d
 docker-compose -f ./twig-with-deps/concourse/compose/e2e.yml run --rm test-runner bash -c "cd twig && npm install && npm run test:ci"
 
