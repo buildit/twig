899ce8643211b7673f9a1af98e993f645e3ae229
Ben Hernandez <ben.hernandez@wipro.com>
2017-10-05 15:05:20 -0600

    test

--- concourse/scripts/run_e2e_tests.sh
+++ concourse/scripts/run_e2e_tests.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 source /docker-lib.sh && start_docker
-docker-compose -f ./twig-with-deps/concourse/compose/e2e.yml up -d
-docker-compose -f ./twig-with-deps/concourse/compose/e2e.yml run --rm test-runner bash -c "cd /twig && npm install && npm run test:e2e:ci -- --base-href http://e2e-web"
+ls -la
+docker-compose -f ./twig/concourse/compose/e2e.yml up -d
+docker-compose -f ./twig/concourse/compose/e2e.yml run --rm test-runner bash -c "cd /twig && npm install && npm run test:e2e:ci -- --base-href http://e2e-web"
 
 # Store the return-code from the test-suite and tear down:
 rc=$?
-docker-compose -f ./twig-with-deps/conc
-ourse/compose/e2e.yml down
+docker-compose -f ./twig/concourse/compose/e2e.yml down
 echo "exit code = $rc "
 exit $rc
