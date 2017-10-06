6abe433f570871e0de3cd41aa25a6b8dbaaa2503
Ben Hernandez <ben.hernandez@wipro.com>
2017-10-05 20:21:54 -0600

    test

--- concourse/scripts/run_e2e_tests.sh
+++ concourse/scripts/run_e2e_tests.sh
@@ -1,6 +1,19 @@
 #!/bin/sh
 
 source /docker-lib.sh && start_docker
+
+docker load -i ng-cli-e2e/image
+docker tag "$(cat ng-cli-e2e/image-id)" "$(cat ng-cli-e2e/repository):$(cat ng-cli-e2e/tag)"
+
+docker load -i couchdb/image
+docker tag "$(cat couchdb/image-id)" "$(cat couchdb/repository):$(cat couchdb/tag)"
+
+docker load -i twig-api/image
+docker tag "$(cat twig-api/image-id)" "$(cat twig-api/repository):$(cat twig-api/tag)"
+
+docker load -i twig-docker-image/image
+docker tag "$(cat twig-docker-image/image-id)" "$(cat twig-docker-image/repository):$(cat twig-docker-image/tag)"
+
 docker-compose -f ./twig/concourse/compose/e2e.yml up -d
 docker-compose -f ./twig/concourse/compose/e2e.yml run --rm test-runner bash -c "cd /twig && npm install && npm run test:e2e:ci -- --base-href http://e2e-web"
 
