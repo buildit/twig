491c5f21c46f6a7024de8dfec3ce9a1b936226f8
Ben Hernandez <ben.hernandez@wipro.com>
2017-10-18 11:20:46 -0600

    testing slack notifications

--- concourse/scripts/get_commit_info.sh
+++ concourse/scripts/get_commit_info.sh
@@ -25,7 +25,7 @@ cat >./commit_info/attachments.json <<EOL
       },
       {
         "title": "Commit",
-        "value": "<$git_commit_url/$SHA|$SHA>",
+        "value": "<$git_commit_url$SHA|$SHA>",
         "short": true
       },
       {
