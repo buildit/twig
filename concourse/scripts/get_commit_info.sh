07059d20ed0ce313f18a5598d9a0f0cce82a3c5e
Ben Hernandez <ben.hernandez@wipro.com>
2017-10-18 09:11:47 -0600

    testing slack notifications

--- concourse/scripts/get_commit_info.sh
+++ concourse/scripts/get_commit_info.sh
@@ -5,7 +5,7 @@ SHA=`git rev-parse --short HEAD`
 MESSAGE=`git show -s --format=%B $SHA`
 cd ..
 cat >./commit_info/text <<EOL
-Commit: <$git_commit_url$SHA|[$SHA]>
+Commit: [<$git_commit_url$SHA|$SHA>]
 Message: "$MESSAGE"
 EOL
 
