88b4713118336b5d930a53489c50714d7f20a025
Ben Hernandez <ben.hernandez@wipro.com>
2017-10-18 08:48:48 -0600

    test

--- concourse/scripts/get_commit_info.sh
+++ concourse/scripts/get_commit_info.sh
@@ -4,7 +4,7 @@ cd twig
 SHA=`git rev-parse --short HEAD`
 MESSAGE=`git show -s --format=%B $SHA`
 cd ..
-cat >./commit_info <<EOL
+cat >./commit_info/text <<EOL
 Commit: <$git_commit_url$SHA|$SHA>
 Message: $MESSAGE
 EOL
