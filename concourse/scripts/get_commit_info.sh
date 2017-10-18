b04573910a16a648360b16d4d5f3a583849d7913
Ben Hernandez <ben.hernandez@wipro.com>
2017-10-18 09:58:36 -0600

    testing slack notifications

--- concourse/scripts/get_commit_info.sh
+++ concourse/scripts/get_commit_info.sh
@@ -1,11 +1,32 @@
 #!/usr/bin/env bash
 
+# Create message
 cd twig
 SHA=`git rev-parse --short HEAD`
 MESSAGE=`git show -s --format=%B $SHA`
 cd ..
 cat >./commit_info/text <<EOL
 Commit: [<$git_commit_url$SHA|$SHA>]
-Message: "$MESSAGE"
+Commit Message: "$MESSAGE"
 EOL
 
+# Create attachment
+PIPELINE_URL="$concourse_ci_url/teams/$BUILD_TEAM_NAME/pipelines/$BUILD_PIPELINE_NAME/jobs/$BUILD_JOB_NAME/builds/$BUILD_NAME"
+TEXT="*Status:* $status\n*Deployment:* $deployment_area\n*Branch:* $branch\nThe build failed at step [<$PIPELINE_URL|$BUILD_JOB_NAME>]"
+if [ "$status" == "FAILED" ];
+then
+  COLOR="danger"
+elif [ "$status" = "SUCCESS" ];
+then
+  COLOR="good"
+else
+  COLOR="#333333"
+fi
+
+cat >./commit_info/attachments <<EOL
+[
+  {
+    "text": "$TEXT",
+    "color": "$COLOR"
+  }
+]
