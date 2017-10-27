cbc6feed90d11124037d64ca58aa6c1f51a047d2
Ben Hernandez <ben.hernandez@wipro.com>
2017-10-27 11:10:56 -0600

    changing buildspec around a bit

---
+++ getCommitDetails.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+SHA=`git rev-parse HEAD`
+SHORT_SHA=`git rev-parse --short HEAD`
+MESSAGE=`git show -s --format=%B $SHA`
+COMMITTER=`git show -s --format=%ce $SHA`
+GIT_REPO="https://github.com/buildit/twig/commit/"
+
+cat >./$1.json <<EOL
+{
+  "sha": "$SHA",
+  "short_sha": "$SHORT_SHA",
+  "message": "$MESSAGE",
+  "committer": "$COMMITTER",
+  "url": "$GIT_REPO$SHA"
+}
