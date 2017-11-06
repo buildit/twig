0e2f307a31a005d66e15cc47f3fd3413e460b8f5
Ben Hernandez <ben.hernandez@wipro.com>
2017-11-06 08:27:33 -0700

    adding better waits for collapsing nodes

--- e2e/journeys/twiglet-environment.e2e-spec.ts
+++ e2e/journeys/twiglet-environment.e2e-spec.ts
@@ -30,39 +30,39 @@ describe('Twiglet Environment Controls', () => {
   describe('Collapsing Nodes', () => {
     it('can collapse a single set of nodes', () => {
       page.twigletGraph.collapseClick('node4-1');
-      browser.waitForAngular();
+      browser.wait(page.twigletGraph.nodeCount.then(count => count === 11), 1000);
       expect(page.twigletGraph.nodeCount).toEqual(11);
     });
 
     it('can flower collapsed nodes', () => {
       page.twigletGraph.collapseClick('node4-1');
-      browser.waitForAngular();
+      browser.wait(page.twigletGraph.nodeCount.then(count => count === 15), 1000);
       expect(page.twigletGraph.nodeCount).toEqual(15);
     });
 
     it('can collapse node 2 generations up', () => {
       page.twigletGraph.collapseClick('node2-2');
-      browser.waitForAngular();
+      browser.wait(page.twigletGraph.nodeCount.then(count => count === 13), 1000);
       expect(page.twigletGraph.nodeCount).toEqual(13);
     });
 
     it('can flower nodes and restore missing generation', () => {
       page.twigletGraph.collapseClick('node2-2');
-      browser.waitForAngular();
+      browser.wait(page.twigletGraph.nodeCount.then(count => count === 15), 1000);
       expect(page.twigletGraph.nodeCount).toEqual(15);
     });
 
     it('can handle collapsing nodes in steps', () => {
       page.twigletGraph.collapseClick('node2-2');
       page.twigletGraph.collapseClick('node4-1');
-      browser.waitForAngular();
+      browser.wait(page.twigletGraph.nodeCount.then(count => count === 9), 1000);
       expect(page.twigletGraph.nodeCount).toEqual(9);
     });
 
     it('can flower nodes that were collapsed insteps', () => {
       page.twigletGraph.collapseClick('node4-1');
       page.twigletGraph.collapseClick('node2-2');
-      browser.waitForAngular();
+      browser.wait(page.twigletGraph.nodeCount.then(count => count === 15), 1000);
       expect(page.twigletGraph.nodeCount).toEqual(15);
     });
 
@@ -70,13 +70,13 @@ describe('Twiglet Environment Controls', () => {
       page.accordion.goToMenu('Environment');
       page.accordion.environmentMenu.toggleByLabel('Cascading Collapse');
       page.twigletGraph.collapseClick('node4-1');
-      browser.waitForAngular();
+      browser.wait(page.twigletGraph.nodeCount.then(count => count === 1), 1000);
       expect(page.twigletGraph.nodeCount).toEqual(1);
     });
 
     it('can cascade flower a set of nodes', () => {
       page.twigletGraph.collapseClick('node4-1');
-      browser.waitForAngular();
+      browser.wait(page.twigletGraph.nodeCount.then(count => count === 15), 1000);
       expect(page.twigletGraph.nodeCount).toEqual(15);
     });
   });
