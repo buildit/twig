2dd7bbd0ea376c655c566fd1cd74201287dadec0
Ben Hernandez <ben.hernandez@wipro.com>
2017-11-07 08:54:56 -0700

    hopefully this fixes the e2e tests

--- e2e/journeys/twiglet-environment.e2e-spec.ts
+++ e2e/journeys/twiglet-environment.e2e-spec.ts
@@ -27,29 +27,45 @@ describe('Twiglet Environment Controls', () => {
     deleteDefaultJsonImportedTwiglet(page);
   });
 
-  const waitTime = 100;
+  const waitTime = 500;
 
   describe('Collapsing Nodes', () => {
     it('can collapse a single set of nodes', () => {
       const nodeNameToClick = 'node4-1';
       page.twigletGraph.collapseClick(nodeNameToClick);
-      browser.wait(() => page.twigletGraph.nodeCount.then(count => count === 11), waitTime)
-      .catch(() => page.twigletGraph.collapseClick(nodeNameToClick));
+      browser.wait(() => page.twigletGraph.nodeCount.then(count => {
+        if (count === 11) {
+          return true;
+        }
+        page.twigletGraph.collapseClick(nodeNameToClick);
+        return false;
+      }), waitTime)
       expect(page.twigletGraph.nodeCount).toEqual(11);
     });
 
     it('can flower collapsed nodes', () => {
       const nodeNameToClick = 'node4-1';
       page.twigletGraph.collapseClick(nodeNameToClick);
-      browser.wait(() => page.twigletGraph.nodeCount.then(count => count === 15), waitTime)
-      .catch(() => page.twigletGraph.collapseClick(nodeNameToClick));
+      browser.wait(() => page.twigletGraph.nodeCount.then(count => {
+        if (count === 15) {
+          return true;
+        }
+        page.twigletGraph.collapseClick(nodeNameToClick);
+        return false;
+      }), waitTime)
       expect(page.twigletGraph.nodeCount).toEqual(15);
     });
 
     it('can collapse node 2 generations up', () => {
       const nodeNameToClick = 'node2-2';
       page.twigletGraph.collapseClick(nodeNameToClick);
-      browser.wait(() => page.twigletGraph.nodeCount.then(count => count === 13), waitTime)
+      browser.wait(() => page.twigletGraph.nodeCount.then(count => {
+        if (count === 13) {
+          return true;
+        }
+        page.twigletGraph.collapseClick(nodeNameToClick);
+        return false;
+      }), waitTime)
       .catch(() => page.twigletGraph.collapseClick(nodeNameToClick));
       expect(page.twigletGraph.nodeCount).toEqual(13);
     });
@@ -57,8 +73,13 @@ describe('Twiglet Environment Controls', () => {
     it('can flower nodes and restore missing generation', () => {
       const nodeNameToClick = 'node2-2';
       page.twigletGraph.collapseClick(nodeNameToClick);
-      browser.wait(() => page.twigletGraph.nodeCount.then(count => count === 15), waitTime)
-      .catch(() => page.twigletGraph.collapseClick(nodeNameToClick));
+      browser.wait(() => page.twigletGraph.nodeCount.then(count => {
+        if (count === 15) {
+          return true;
+        }
+        page.twigletGraph.collapseClick(nodeNameToClick);
+        return false;
+      }), waitTime)
       expect(page.twigletGraph.nodeCount).toEqual(15);
     });
 
@@ -66,11 +87,21 @@ describe('Twiglet Environment Controls', () => {
       const firstNodeNameToClick = 'node2-2';
       const secondNodeNameToClick = 'node4-1';
       page.twigletGraph.collapseClick(firstNodeNameToClick);
-      browser.wait(() => page.twigletGraph.nodeCount.then(count => count === 13), waitTime)
-      .catch(() => page.twigletGraph.collapseClick(firstNodeNameToClick));
+      browser.wait(() => page.twigletGraph.nodeCount.then(count => {
+        if (count === 13) {
+          return true;
+        }
+        page.twigletGraph.collapseClick(firstNodeNameToClick);
+        return false;
+      }), waitTime)
       page.twigletGraph.collapseClick(secondNodeNameToClick);
-      browser.wait(() => page.twigletGraph.nodeCount.then(count => count === 9), waitTime)
-      .catch(() => page.twigletGraph.collapseClick(secondNodeNameToClick));
+      browser.wait(() => page.twigletGraph.nodeCount.then(count => {
+        if (count === 9) {
+          return true;
+        }
+        page.twigletGraph.collapseClick(secondNodeNameToClick);
+        return false;
+      }), waitTime)
       expect(page.twigletGraph.nodeCount).toEqual(9);
     });
 
@@ -78,11 +109,21 @@ describe('Twiglet Environment Controls', () => {
       const firstNodeNameToClick = 'node4-1';
       const secondNodeNameToClick = 'node2-2';
       page.twigletGraph.collapseClick(firstNodeNameToClick);
-      browser.wait(() => page.twigletGraph.nodeCount.then(count => count === 13), waitTime)
-      .catch(() => page.twigletGraph.collapseClick(firstNodeNameToClick));
+      browser.wait(() => page.twigletGraph.nodeCount.then(count => {
+        if (count === 13) {
+          return true;
+        }
+        page.twigletGraph.collapseClick(firstNodeNameToClick);
+        return false;
+      }), waitTime)
       page.twigletGraph.collapseClick(secondNodeNameToClick);
-      browser.wait(() => page.twigletGraph.nodeCount.then(count => count === 15), waitTime)
-      .catch(() => page.twigletGraph.collapseClick(secondNodeNameToClick));
+      browser.wait(() => page.twigletGraph.nodeCount.then(count => {
+        if (count === 15) {
+          return true;
+        }
+        page.twigletGraph.collapseClick(secondNodeNameToClick);
+        return false;
+      }), waitTime)
       expect(page.twigletGraph.nodeCount).toEqual(15);
     });
 
@@ -91,16 +132,26 @@ describe('Twiglet Environment Controls', () => {
       page.accordion.goToMenu('Environment');
       page.accordion.environmentMenu.toggleByLabel('Cascading Collapse');
       page.twigletGraph.collapseClick(nodeNameToClick);
-      browser.wait(() => page.twigletGraph.nodeCount.then(count => count === 1), waitTime)
-      .catch(() => page.twigletGraph.collapseClick(nodeNameToClick));
+      browser.wait(() => page.twigletGraph.nodeCount.then(count => {
+        if (count === 1) {
+          return true;
+        }
+        page.twigletGraph.collapseClick(nodeNameToClick);
+        return false;
+      }), waitTime)
       expect(page.twigletGraph.nodeCount).toEqual(1);
     });
 
     it('can cascade flower a set of nodes', () => {
       const nodeNameToClick = 'node4-1';
       page.twigletGraph.collapseClick(nodeNameToClick);
-      browser.wait(() => page.twigletGraph.nodeCount.then(count => count === 15), waitTime)
-      .catch(() => page.twigletGraph.collapseClick(nodeNameToClick));
+      browser.wait(() => page.twigletGraph.nodeCount.then(count => {
+        if (count === 15) {
+          return true;
+        }
+        page.twigletGraph.collapseClick(nodeNameToClick);
+        return false;
+      }), waitTime)
       expect(page.twigletGraph.nodeCount).toEqual(15);
     });
   });
